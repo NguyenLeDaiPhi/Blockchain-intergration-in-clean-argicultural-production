a8e5074a27da6f5f7a1d985c9774bf083be459ac
TuanNguyen-dev28 <nat281205@gmail.com>
2026-01-10 12:31:43 +0700

    delete farm-management in farm product

--- services/farm-production-service/src/main/java/com/bicap/farm_management/repository/MarketplaceProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.bicap.farm_management.repository;
-
-import com.bicap.farm_management.entity.MarketplaceProduct;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MarketplaceProductRepository extends JpaRepository<MarketplaceProduct, Long> {
-}
