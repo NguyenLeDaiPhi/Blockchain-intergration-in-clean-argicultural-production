e5e0b90efcec04b59571e1687765546f2d765dc2
TuanNguyen-dev28 <nat281205@gmail.com>
2026-01-10 14:19:16 +0700

    push farm-prodution

--- services/farm-production-service/src/main/java/com/bicap/farm_management/dto/BlockchainMessage.java
+++ services/farm-production-service/src/main/java/com/bicap/farm_management/dto/BlockchainMessage.java
@@ -1,9 +1,10 @@
 package com.bicap.farm_management.dto;
 
+import java.io.Serializable;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import java.io.Serializable;
 
 @Data
 @AllArgsConstructor
