4d7ae5e6acdb0ba4a003a46c004e2c1c11f211c7
TuanNguyen-dev28 <nat281205@gmail.com>
2026-01-18 01:09:14 +0700

    fix lỗi trùng cổng

--- infrastructure/init.sql
+++ infrastructure/init.sql
@@ -4,5 +4,7 @@ CREATE DATABASE IF NOT EXISTS farm_management_db;
 -- 2. Database cho Blockchain Adapter Service
 CREATE DATABASE IF NOT EXISTS bicap_blockchain_db;
 
+-- 3. Database cho Shipping Manager Service
+CREATE DATABASE IF NOT EXISTS shipping_db;
 GRANT ALL PRIVILEGES ON *.* TO 'root'@'%';
 FLUSH PRIVILEGES;
