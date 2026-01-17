70b2a67d0eb569a14e95adb797c1739f25e5f0bb
Nguyen Le Dai Phi <nguyenledaiphi0252005@gmail.com>
2026-01-17 20:15:43 +0700

    Delete the old files

--- services/trading-order-service/trading_order_service/src/main/java/com/bicap/trading_order_service/repository/OrderFeedbackRepository.java
+++
@@ -1,7 +0,0 @@
-package com.bicap.trading_order_service.repository;
-
-import com.bicap.trading_order_service.entity.OrderFeedback;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderFeedbackRepository extends JpaRepository<OrderFeedback, Long> {
-}
