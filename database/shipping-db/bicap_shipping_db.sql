d5221536b4e9162bcb77120b63e871a9556b4275
TuanNguyen-dev28 <nat281205@gmail.com>
2026-01-20 17:32:32 +0700

    push project after repair progeties for Tuan

---
+++ database/shipping-db/bicap_shipping_db.sql
@@ -0,0 +1,6 @@
+-- Shipping Manager Database
+-- Database sẽ được tự động tạo bởi JPA với ddl-auto=update
+-- File này để đảm bảo database được tạo khi container khởi động
+
+CREATE DATABASE IF NOT EXISTS shipping_db;
+USE shipping_db;
